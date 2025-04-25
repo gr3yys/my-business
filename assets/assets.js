d189816cc2894a00e1521bb344eba3f3e647a607
grey <greys0@icloud.com>
2025-04-25 11:10:39 -0400

    Added images/icons.

---
+++ assets/assets.js
@@ -0,0 +1,13 @@
+import shopping_bag from './shopping-bag.png'
+import search from './search.png'
+import menu_black from './menu-black.png'
+import close_black from './close-black.png'
+import home_icon from './home.png'
+
+export const assets = {
+    shopping_bag,
+    search,
+    menu_black,
+    close_black,
+    home_icon
+}
