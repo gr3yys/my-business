f576bb5b1fe31967bada0a31f07ca5b225793afe
grey <greys0@icloud.com>
2025-04-25 11:11:03 -0400

    Added credits for the icons.

---
+++ public/attributions/attributions.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const attributions = () => {
+  return (
+    <div>
+        <h2>Asset Credits</h2>
+        <p>
+            <a href="https://www.flaticon.com/free-icons/basket" title="basket icons">Basket icons created by Karacis - Flaticon</a>
+        </p>
+        <p>
+          <a href="https://www.flaticon.com/free-icons/minimal" title="minimal icons">Minimal icons created by Ze Leah - Flaticon</a>
+        </p>
+        <p>
+          <a href="https://www.flaticon.com/free-icons/search" title="search icons">Search icons created by Catalin Fertu - Flaticon</a>
+        </p>
+    </div>
+  )
+}
+
+export default attributions
